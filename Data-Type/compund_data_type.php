a112640e0d21842eb74738d3e92645a9eb05613b
tanjil-hosain <tanjilh85@gmail.com>
2026-03-05 11:35:33 +0600

    Array & object

---
+++ Data-Type/compund_data_type.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // Array
+    $a = array("a","b","C");
+    echo $a;
+    echo"<br>";
+    var_dump($a);
+
+    // object
+    class Student1{
+        public $name= "Mosha";
+    } $obj2 = new Student1();
+    var_dump($obj2);
+    ?>
+</body>
+</html>
