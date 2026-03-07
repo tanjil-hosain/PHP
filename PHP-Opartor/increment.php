1f987f1993bba465c7eaf76835101e8b45ce9177
tanjil-hosain <tanjilh85@gmail.com>
2026-03-07 11:55:02 +0600

    increment

---
+++ PHP-Opartor/increment.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // pre
+
+    $x=10;
+    echo "<br>";
+    echo ++$x;
+    ?>
+
+     <?php
+    // pre
+
+    $y=110;
+    echo "<br>";
+    echo $y++;
+    ?>
+</body>
+</html>
