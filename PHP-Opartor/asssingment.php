18c8f76bfa276897f20ceeacab605431ae1b2712
tanjil-hosain <tanjilh85@gmail.com>
2026-03-07 11:28:22 +0600

    asingment-oparator

---
+++ PHP-Opartor/asssingment.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assingment</title>
+</head>
+<body>
+    <?php
+    $a=10;
+    $a+=20;
+    echo $a;
+    ?>
+</body>
+</html>
