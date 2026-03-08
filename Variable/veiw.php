46a268c45fdd9c9f78e19589654c1abaf61a1b6c
tanjil-hosain <tanjilh85@gmail.com>
2026-03-08 10:58:09 +0600

    form

---
+++ Variable/veiw.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="#" method="post">
+        User Name: <br>
+        <input type="text" name="uname"> <br>
+        <input type="submit" value="Submit">
+
+
+    </form>
+
+    <?php
+    echo $_REQUEST['uname'];
+    ?>
+</body>
+</html>
