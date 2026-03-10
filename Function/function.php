ff57b03eacbd428490145ae10ad684dd785d0648
tanjil-hosain <tanjilh85@gmail.com>
2026-03-10 10:04:55 +0600

    function

---
+++ Function/function.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- user_Fucntiom -->
+    <?php
+    function test($tanjil)
+    {
+        echo "He Is a Gentel Men!! $tanjil";
+    }
+    test("Yes i show it")
+    ?>
+
+    <!-- Annonimus Function -->
+     <?php
+     
+     ?>
+</body>
+</html>
