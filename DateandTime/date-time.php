a32922aed451195a43056e19735e3cbf576f469c
tanjil-hosain <tanjilh85@gmail.com>
2026-03-11 10:09:17 +0600

    date and time

---
+++ DateandTime/date-time.php
@@ -0,0 +1,40 @@
+<?php
+
+echo time();   // 1773201098
+echo "<br>";
+
+echo date("d");  // 11
+echo "<br>";
+
+echo date("D"); // Wed
+echo "<br>";
+
+echo date("m");  // 03
+echo "<br>";
+
+
+echo date("M"); // Mar
+echo "<br>";
+
+echo date("Y"); //2026
+echo "<br>";
+
+echo date("y"); //26
+echo "<br>";
+
+echo date("n");  //3 this is month numuric
+echo "<br>";
+
+echo date("L");   //leep year
+echo "<br>";
+
+echo date("F");  // full month name 
+echo "<br>";
+
+
+echo date("d/m/y");  //full date
+echo "<br>";
+
+?>
+
+
