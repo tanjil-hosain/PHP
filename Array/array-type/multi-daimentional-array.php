cde2cd439f5a917bbe04b8dc6effd4b13d7b24ff
tanjil-hosain <tanjilh85@gmail.com>
2026-03-12 10:38:16 +0600

    listing style in array

--- Array/array-type/multi-daimentional-array.php
+++ Array/array-type/multi-daimentional-array.php
@@ -1,4 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .list-style{
+            color: green;
+            list-style-type: square;
+
+        }
+    </style>
+</head>
+<body>
+    <?php
 $letter = [
     ["A", "B","C","D"],
     ["E","F","G","H"],
@@ -15,3 +30,20 @@ for($i=0; $i<count($letter); $i++){
     }
     echo "</ul>";
 }
+
+$people= [
+    ["A", "B","C","D"],
+    ["E","F","G","H"],
+    [1,2,3,4,5]
+];
+
+foreach($people as $ordinary){
+    echo "<ul class = 'list-style'>";
+    foreach($ordinary as $genarel){
+        echo "<li >" .$genarel . "</li>";
+    }
+    echo "</ul>";
+}
+?>
+</body>
+</html>
