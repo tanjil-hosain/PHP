f918b37fe4ba487217d47e10d985a0c9565ed66f
tanjil-hosain <tanjilh85@gmail.com>
2026-03-13 22:47:32 +0600

    include && include_once

---
+++ include&&requird/Include/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+   <?php
+//    include('header.php');       // include sudu jukto ekti file joto bar deya hobe toto bar eti run korbe
+//    include('header.php');
+//    include('header.php');
+//    include('header.php');
+      include_once('header.php');   // r jodi Include_once use kora hoy tahole ekti file ba bar bebohar kore just ekbar run korbe etai hocche include r include_once er modde parthokko
+
+      include_once('header.php'); // arekta + ponit hocche include bebohar korle kono erore thakle seta bade sob gula run korbe!!!!
+      include_once('header.php');
+
+
+   ?>
+    <h3>THis is my Frist Inculde</h3>
+    <?php
+    include('footer.php');
+    include('footer.php');
+    include('footer.php');
+    include('footer.php');
+    ?>
+</body>
+</html>
