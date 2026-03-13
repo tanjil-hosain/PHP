8799fdb9ade39e37362b8dc10bf59169c4801f1b
tanjil-hosain <tanjilh85@gmail.com>
2026-03-13 22:59:26 +0600

    requir && requir_once

---
+++ include&&requird/required/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    require('header.php');
+    require('headeeeer.php'); // requir er use korle file add hoy but kono erore thakle next kono program run kore na!!      eta hocche include r requir er moddhe parthokko!!
+
+    require('header.php');  // requir_once use korle just ekbar run kore
+    require('header.php');
+
+    ?>
+
+
+    <h6>This is Required </h1>
+
+    <?php
+    require('footer.php');
+    ?>
+</body>
+</html>
