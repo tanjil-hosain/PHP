d3b182e13cfb02aa8cfec6c63f8277beb5dea767
tanjil-hosain <tanjilh85@gmail.com>
2026-03-31 22:41:30 +0600

    file_get_contetn

--- File Opareting System/php-file-read.php
+++ File Opareting System/php-file-read.php
@@ -1,4 +1,9 @@
 <?php
-$result = fopen("frist.txt", "r") or die("file is not found");
-echo fread($result, filesize("frist.txt"));
-fclose($result);
+// $result = fopen("frist.txt", "r") or die("file is not found");
+// echo fread($result, filesize("frist.txt"));
+// fclose($result);
+
+echo readfile("frist.txt"); // ertar maddome korle charecter size porjonto asbe!
+
+$r = file_get_contents("frist.txt"); // etar maddome file anay jay
+echo $r;
