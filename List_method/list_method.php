2cadac4006f977a254659a35dc0bb34ba646e7f8
tanjil-hosain <tanjilh85@gmail.com>
2026-04-02 10:04:31 +0600

    list-method

---
+++ List_method/list_method.php
@@ -0,0 +1,11 @@
+<?php
+$a=[
+   [1,"asad", 40, "asad@gmail.com", "46473"],
+   [2,"asad", 40, "asad@gmail.com", "46473"],
+   [3,"asad", 40, "asad@gmail.com", "46473"],
+   [4,"asad", 40, "asad@gmail.com", "46473"]
+];
+
+foreach($a as list($id, $name, $age)){
+    echo "$name | $age ";
+}
