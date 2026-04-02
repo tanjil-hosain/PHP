6e1b5b1811d54c3847fcda74bd6bf7c0146883a5
tanjil-hosain <tanjilh85@gmail.com>
2026-04-02 12:51:26 +0600

    provlemsolving

---
+++ Exam_problem_solving/Prime_number.php
@@ -0,0 +1,30 @@
+<form action="#" method="post">
+    Enter Your Number: <br>
+    <input type="text" name="marks"><br> <br>
+    <input type="submit" name="submit" value="Check Prime NUmber">
+</form>
+<?php
+if(isset($_POST['submit'])){
+    $marks= $_POST['marks'];
+    $count=0;
+    {
+        if($marks <=1){
+            echo $marks. " The number is not Prime Or Compositor";
+        }
+        else{
+            for($i= 2; $i < $marks; $i++){
+                if($marks % $i === 0){
+                    $count++;
+                    break;
+                }
+            }
+            if($count==0){
+                echo $marks. " It's Prime Number";
+            } else{
+                echo $marks. "  It's not a Prime Number";
+            }
+        }
+
+    }
+}
+?>
