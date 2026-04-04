9f02e61e7bd36399df93e54b2ef16f8a73b4ab8e
tanjil-hosain <tanjilh85@gmail.com>
2026-04-04 21:09:02 +0600

    car_class

---
+++ OOP/Name_scap/Project_car/car_class.php
@@ -0,0 +1,10 @@
+<?php
+class Car {
+    public $name = "BMW";
+    public $model = "i80";
+    public function carInfo(){
+        echo "This is ". $this->name;
+    }
+}
+$result = new Car();
+$result->carInfo();
