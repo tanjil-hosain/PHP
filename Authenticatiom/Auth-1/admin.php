ae0d904508461e765755a966b2c861bb7fa99c63
tanjil-hosain <tanjilh85@gmail.com>
2026-04-11 11:34:41 +0600

    login from data-txt-file

---
+++ Authenticatiom/Auth-1/admin.php
@@ -0,0 +1,53 @@
+<?php 
+  
+  if(isset($_POST["btnLogin"])){
+	  
+	  $username=$_POST["txtUsername"];
+	  $password=$_POST["txtPassword"];
+	  
+	  $file=file("data.txt");
+	  
+	  foreach($file as $line){
+		  
+		  list($_username,$_password)=explode(",",$line);
+		  
+		  if(trim($_username)==$username && trim($_password)==$password){
+			
+			 header("location:main.php");
+			  
+		  }else{
+			  
+			  $msg="Username or Password is incorrect!";
+		  }
+		  
+	  }
+	  
+  }
+
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+ <?php
+   echo isset($msg)?$msg:"";
+ ?>
+ <form action="#" method="post">
+   <div>
+    Username<br/>
+    <input type="text" name="txtUsername" />
+   </div>
+   <div>
+    Password<br/>
+    <input type="password" name="txtPassword" />
+   </div>
+   <div>
+    <input type="submit" value="Log In" name="btnLogin" />
+   </div>
+ </form>
+</body>
+</html>
