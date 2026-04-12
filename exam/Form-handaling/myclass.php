75adfb0702468d68df81c6e7199918b11d245815
tanjil-hosain <tanjilh85@gmail.com>
2026-04-12 10:00:31 +0600

    half

---
+++ exam/Form-handaling/myclass.php
@@ -0,0 +1,11 @@
+<?php
+class Person {
+    public $name;
+    public $address;
+
+    public function __construct($name, $address) {
+        $this->name = $name;
+        $this->address = $address;
+    }
+}
+?>
