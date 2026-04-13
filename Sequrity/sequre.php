2ae05463fc30a62207ee723f8523e5a213354553
tanjil-hosain <tanjilh85@gmail.com>
2026-04-13 11:47:20 +0600

    based64_encode && decode

--- Sequrity/sequre.php
+++ Sequrity/sequre.php
@@ -4,7 +4,17 @@ $password = "123";
 echo md5($password);
 echo "<br>";
 
-echo hash('SHA512', "admin@");
+echo hash('SHA512', "admin@"); 
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+
+$store="1234";
+// echo base64_encode($store);
+
+echo base64_decode("MTIzNA==");
 
 
 
