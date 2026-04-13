cef5e4a901a8610b5c89391f2baeb6b328814674
tanjil-hosain <tanjilh85@gmail.com>
2026-04-13 12:54:01 +0600

    password_hash

--- Sequrity/sequre.php
+++ Sequrity/sequre.php
@@ -15,6 +15,14 @@ $store="1234";
 // echo base64_encode($store);
 
 echo base64_decode("MTIzNA==");
+echo "<br>";
+$pass="adim";
+$show=password_hash("$pass", PASSWORD_DEFAULT);
+if(password_verify($pass, $show)){
+    echo "valid";
+} else{
+    echo"invalid";
+}
 
 
 
